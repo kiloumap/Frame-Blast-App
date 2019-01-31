e9379539fa05bcc0b15b0a8669170a3099471e4b
Martinez Laurent <laurent.martinez@ynov.com>
2019-01-31 19:02:17 +0100

    landing page ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,37 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    loadChildren: './home/home.module#HomePageModule'
-  },
-  {
-    path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
-  }
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        loadChildren: './home/home.module#HomePageModule'
+    },
+    {
+        path: 'list',
+        loadChildren: './list/list.module#ListPageModule'
+    },
+    {
+        path: 'events',
+        loadChildren: './events/events.module#EventsPageModule'
+    },
+    {
+        path: 'teams',
+        loadChildren: './teams/teams.module#TeamsPageModule'
+    },
+    {
+        path: 'infos',
+        loadChildren: './infos/infos.module#InfosPageModule'
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
+export class HomePage {
+    private eventTime = new Date('2019-05-10T10:00:00').getTime();
+    private duration = moment.duration(this.eventTime - new Date().getTime(), 'milliseconds');
+    private interval = 1000;
+
+    constructor() {
+        setInterval( () => {
+            this.duration = moment.duration(this.eventTime - new Date().getTime(), 'milliseconds');
+            // $('.countdown').text(duration.hours() + ":" + duration.minutes() + ":" + duration.seconds())
+        }, this.interval);
+    }
 }
